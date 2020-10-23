71606470bc6cd3ee39624b77bbc0e5e403c9114d
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 19:40:31 +0600

    added -> Koin DI

--- app/src/main/java/com/alis/lotcion/App.kt
+++ app/src/main/java/com/alis/lotcion/App.kt
@@ -1,10 +1,17 @@
 package com.alis.lotcion
 
 import android.app.Application
+import com.alis.lotcion.di.lotcionModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(lotcionModule)
+        }
     }
 }
--- app/src/main/java/com/alis/lotcion/di/Modules.kt
+++ app/src/main/java/com/alis/lotcion/di/Modules.kt
@@ -1,7 +1,15 @@
 package com.alis.lotcion.di
 
+import com.alis.lotcion.ui.authentication.signin.SignInViewModel
+import com.alis.lotcion.ui.authentication.signup.SignUpViewModel
+import com.alis.lotcion.ui.authentication.verifycode.VerifyCodeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 var lotcionModule = module {
+
+    viewModel { SignInViewModel() }
+    viewModel { SignUpViewModel() }
+    viewModel { VerifyCodeViewModel() }
 
 }
