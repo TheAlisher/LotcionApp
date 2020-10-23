d8da637361ce9ee054849d146bc6b26f3e462fb7
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 21:46:50 +0600

    added -> Toast extension & HideKeyboard extension

--- app/src/main/java/com/alis/lotcion/extensions/UIExtension.kt
+++ app/src/main/java/com/alis/lotcion/extensions/UIExtension.kt
@@ -1,6 +1,10 @@
 package com.alis.lotcion.extensions
 
+import android.app.Activity
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 
 fun View.visible() {
     visibility = View.VISIBLE
@@ -12,4 +16,29 @@ fun View.invisible() {
 
 fun View.gone() {
     visibility = View.GONE
+}
+
+fun showToastShort(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+fun showToastLong(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
+
+fun showToastShort(context: Context, message: Int) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+fun showToastLong(context: Context, message: Int) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
+
+fun hideKeyboard(activity: Activity) {
+    val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    var view = activity.currentFocus
+    if (view == null) {
+        view = View(activity)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
