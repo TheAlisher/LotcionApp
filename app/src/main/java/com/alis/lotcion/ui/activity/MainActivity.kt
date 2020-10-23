5f6b468f2a5b39afd4cbb80cfbbdb43953b14a24
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 06:46:51 +0600

    moved -> MainActivity.kt

--- app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.alis.lotcion
+package com.alis.lotcion.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alis.lotcion.R
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
