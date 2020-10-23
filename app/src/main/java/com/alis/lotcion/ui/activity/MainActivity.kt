1ab5bb0d6afe07b455ea030d8a1e80146b0cf984
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 07:47:59 +0600

    added -> Navigation with BottomNav

--- app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
@@ -2,12 +2,39 @@ package com.alis.lotcion.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.alis.lotcion.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavController()
+    }
+
+    private fun setUpNavController() {
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNav.setupWithNavController(navController)
+        addNavControllerDestinationChangedListener()
+    }
+
+    private fun addNavControllerDestinationChangedListener() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.navigation_home, R.id.navigation_add_lot, R.id.navigation_profile -> {
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                }
+            }
+        }
     }
 }
