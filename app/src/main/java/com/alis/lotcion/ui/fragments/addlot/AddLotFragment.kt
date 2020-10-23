4657f8a2ef07568ec5349436e7ee9a5e0ac22105
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 07:45:07 +0600

    Initial

---
+++ app/src/main/java/com/alis/lotcion/ui/fragments/addlot/AddLotFragment.kt
@@ -0,0 +1,18 @@
+package com.alis.lotcion.ui.fragments.addlot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alis.lotcion.R
+
+class AddLotFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_lot, container, false)
+    }
+}
