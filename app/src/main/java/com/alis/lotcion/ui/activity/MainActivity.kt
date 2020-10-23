1f635552b29d2dbda339f35d1871656e1bc16481
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 20:45:22 +0600

    added -> navigationDestinationChangedListener logic

--- app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
@@ -2,15 +2,22 @@ package com.alis.lotcion.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.alis.lotcion.R
+import com.alis.lotcion.extensions.gone
+import com.alis.lotcion.extensions.visible
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var bottomNav: BottomNavigationView
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavController() {
-        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNav = findViewById(R.id.bottom_nav)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
@@ -31,10 +38,35 @@ class MainActivity : AppCompatActivity() {
     private fun addNavControllerDestinationChangedListener() {
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when (destination.id) {
-                R.id.navigation_home, R.id.navigation_add_lot, R.id.navigation_profile -> {
-                    window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                R.id.signInFragment,
+                R.id.signUpFragment,
+                R.id.verifyCodeFragment,
+                R.id.editProfileFragment,
+                R.id.settingsFragment -> {
+                    bottomNav.gone()
+                }
+                R.id.navigation_home,
+                R.id.navigation_add_lot,
+                R.id.navigation_profile -> {
+                    bottomNav.visible()
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                }
+                R.id.lotFragment -> {
+                    bottomNav.gone()
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (isAuthentication()) {
+            navController.navigate(R.id.signInFragment)
+        }
+    }
+
+    private fun isAuthentication(): Boolean {
+        return FirebaseAuth.getInstance().currentUser == null
     }
 }
