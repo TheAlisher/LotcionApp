3e0122837232b05cd3a2ad156fc2545ed7e53f50
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 19:48:51 +0600

    added -> all ViewModels

--- app/src/main/java/com/alis/lotcion/di/Modules.kt
+++ app/src/main/java/com/alis/lotcion/di/Modules.kt
@@ -3,6 +3,12 @@ package com.alis.lotcion.di
 import com.alis.lotcion.ui.authentication.signin.SignInViewModel
 import com.alis.lotcion.ui.authentication.signup.SignUpViewModel
 import com.alis.lotcion.ui.authentication.verifycode.VerifyCodeViewModel
+import com.alis.lotcion.ui.fragments.addlot.AddLotViewModel
+import com.alis.lotcion.ui.fragments.home.HomeViewModel
+import com.alis.lotcion.ui.fragments.home.lots.LotsViewModel
+import com.alis.lotcion.ui.fragments.profile.ProfileViewModel
+import com.alis.lotcion.ui.fragments.profile.editprofile.EditProfileViewModel
+import com.alis.lotcion.ui.lot.LotViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,5 +17,10 @@ var lotcionModule = module {
     viewModel { SignInViewModel() }
     viewModel { SignUpViewModel() }
     viewModel { VerifyCodeViewModel() }
-
+    viewModel { HomeViewModel() }
+    viewModel { AddLotViewModel() }
+    viewModel { ProfileViewModel() }
+    viewModel { LotsViewModel() }
+    viewModel { EditProfileViewModel() }
+    viewModel { LotViewModel() }
 }
