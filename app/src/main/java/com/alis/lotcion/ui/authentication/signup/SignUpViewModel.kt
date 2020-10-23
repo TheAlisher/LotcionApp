2204fe4669e4bb8b8c6fac17498cc0e1e4d23a41
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 19:39:45 +0600

    Created

---
+++ app/src/main/java/com/alis/lotcion/ui/authentication/signup/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.alis.lotcion.ui.authentication.signup
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/alis/lotcion/ui/authentication/verifycode/VerifyCodeViewModel.kt
@@ -0,0 +1,7 @@
+package com.alis.lotcion.ui.authentication.verifycode
+
+import androidx.lifecycle.ViewModel
+
+class VerifyCodeViewModel : ViewModel() {
+
+}
