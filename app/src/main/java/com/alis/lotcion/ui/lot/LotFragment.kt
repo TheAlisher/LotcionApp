c695c6dfa6e6a4a412e0931227159dcccc54f13a
macbookpro <abenov2005.uzer@gmail.com>
2020-10-25 02:43:48 +0600

    added -> start method

--- app/src/main/java/com/alis/lotcion/ui/lot/LotFragment.kt
+++ app/src/main/java/com/alis/lotcion/ui/lot/LotFragment.kt
@@ -1,14 +1,27 @@
 package com.alis.lotcion.ui.lot
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.alis.lotcion.R
+import com.alis.lotcion.models.Lot
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LotFragment : Fragment() {
+
+    companion object {
+        private var item: Lot? = null
+        fun start(activity: Activity, action: Int, item: Lot) {
+            this.item = item
+            Navigation
+                .findNavController(activity, R.id.nav_host_fragment)
+                .navigate(action)
+        }
+    }
 
     private val viewModel by viewModel<LotViewModel>()
 
