912a186c56aac9cc060356fc06a6b8ff74ea579c
macbookpro <abenov2005.uzer@gmail.com>
2020-10-25 02:26:10 +0600

    added -> Lots recycler

--- app/src/main/java/com/alis/lotcion/ui/fragments/home/lots/LotsFragment.kt
+++ app/src/main/java/com/alis/lotcion/ui/fragments/home/lots/LotsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.alis.lotcion.R
+import com.alis.lotcion.adapters.LotsAdapter
+import com.alis.lotcion.models.Lot
+import kotlinx.android.synthetic.main.fragment_lots.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LotsFragment : Fragment() {
@@ -16,6 +20,8 @@ class LotsFragment : Fragment() {
 
     private val viewModel by viewModel<LotsViewModel>()
 
+    private lateinit var lotsAdapter: LotsAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,5 +31,158 @@ class LotsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        createLotsRecycler()
+        setupListeners()
+        fetchLots()
+    }
+
+    private fun createLotsRecycler() {
+        lotsAdapter = LotsAdapter()
+        recycler_lots.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = lotsAdapter
+        }
+    }
+
+    private fun setupListeners() {
+        lotsAdapter.setOnItemClickListener(object : LotsAdapter.OnItemClickListener {
+            override fun onLotItemClick(item: Lot) {
+
+            }
+        })
+    }
+
+    private fun fetchLots() {
+        lotsAdapter.clear()
+        var lot: Lot? = null
+        when (requireArguments().getInt(ARG_TAB_POSITION)) {
+            0 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            1 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            2 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            3 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            4 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            5 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            6 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            7 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            8 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+            9 -> {
+                lot = Lot(
+                    R.drawable.ic_launcher_background,
+                    "ЛОТ",
+                    "ОПИСАНИЕ ЛОТА",
+                    1200,
+                    3400,
+                    "21ч : 34м : 3с",
+                    null,
+                    false,
+                )
+            }
+        }
+        lotsAdapter.add(lot!!)
+        lotsAdapter.add(lot)
+        lotsAdapter.add(lot)
+        lotsAdapter.add(lot)
+        lotsAdapter.add(lot)
+        lotsAdapter.add(lot)
     }
 }
