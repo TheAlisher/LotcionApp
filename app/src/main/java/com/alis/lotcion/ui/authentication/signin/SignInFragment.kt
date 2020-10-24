2d4e62bc94ec81c671e9eed90e67623917b05d66
macbookpro <abenov2005.uzer@gmail.com>
2020-10-24 13:18:12 +0600

    added -> Google Auth

--- app/src/main/java/com/alis/lotcion/ui/authentication/signin/SignInFragment.kt
+++ app/src/main/java/com/alis/lotcion/ui/authentication/signin/SignInFragment.kt
@@ -1,6 +1,8 @@
 package com.alis.lotcion.ui.authentication.signin
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,14 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.alis.lotcion.R
+import com.alis.lotcion.ui.fragments.home.HomeFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.fragment_sign_in.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -15,16 +25,9 @@ class SignInFragment : Fragment() {
 
     private val viewModel by viewModel<SignInViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        overrideOnBackPressed()
-    }
-
-    private fun overrideOnBackPressed() {
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            requireActivity().finish()
-        }
-    }
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 33
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,8 +38,10 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
 
         setupListeners()
+        configureGoogleSignIn()
     }
 
     private fun setupListeners() {
@@ -47,13 +52,13 @@ class SignInFragment : Fragment() {
 
     private fun clickSignInWithGoogle() {
         button_sign_in_with_google.setOnClickListener {
-
+            signIn()
         }
     }
 
     private fun clickSignIn() {
         button_sign_in.setOnClickListener {
-
+            HomeFragment.start(requireActivity(), R.id.action_signInFragment_to_navigation_home)
         }
     }
 
@@ -61,5 +66,56 @@ class SignInFragment : Fragment() {
         button_sign_in_don_t_have_account.setOnClickListener {
             findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
         }
+    }
+
+    private fun configureGoogleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("GoogleAuth", "firebaseAuthWithGoogle:" + account.id)
+
+                firebaseAuthWithGoogle(account.idToken!!)
+
+            } catch (e: ApiException) {
+                Log.w("GoogleAuth", "Google sign in failed", e)
+
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d("GoogleAuth", "signInWithCredential:success")
+
+                    HomeFragment.start(
+                        requireActivity(),
+                        R.id.action_signInFragment_to_navigation_home
+                    )
+
+                } else {
+                    Log.w("GoogleAuth", "signInWithCredential:failure", task.exception)
+
+                }
+            }
     }
 }
