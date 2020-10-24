bca321c867653abb2ff7063085d35ce760257ab0
macbookpro <abenov2005.uzer@gmail.com>
2020-10-25 02:18:54 +0600

    added -> all variable optional

--- app/src/main/java/com/alis/lotcion/models/Lot.kt
+++ app/src/main/java/com/alis/lotcion/models/Lot.kt
@@ -1,12 +1,12 @@
 package com.alis.lotcion.models
 
 data class Lot(
-    var image: Int,
-    var name: String,
-    var description: String,
-    var startingPrice: Int,
-    var finalPrice: Int,
-    var timeLeft: String,
-    var bidders: MutableList<Bidder>,
+    var image: Int?,
+    var name: String?,
+    var description: String?,
+    var startingPrice: Int?,
+    var finalPrice: Int?,
+    var timeLeft: String?,
+    var bidders: MutableList<Bidder>?,
     var isLiked: Boolean = false
 )
