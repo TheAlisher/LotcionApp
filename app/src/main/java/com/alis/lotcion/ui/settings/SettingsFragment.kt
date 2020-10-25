78b8d9086f073c92a9f44791130394519d99b653
macbookpro <abenov2005.uzer@gmail.com>
2020-10-25 21:00:31 +0600

    added -> SignOut

--- app/src/main/java/com/alis/lotcion/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/alis/lotcion/ui/settings/SettingsFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.alis.lotcion.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SettingsFragment : Fragment() {
@@ -21,5 +27,14 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        button_settings_sign_out.setOnClickListener {
+            Firebase.auth.signOut()
+            findNavController().navigate(R.id.action_settingsFragment_to_signInFragment)
+        }
     }
 }
