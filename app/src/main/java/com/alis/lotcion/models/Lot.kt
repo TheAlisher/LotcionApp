bc71a30ec4ae7e95c3783da96adda1b4e0603d7c
macbookpro <abenov2005.uzer@gmail.com>
2020-10-26 22:14:42 +0600

    added -> getMockData method

--- app/src/main/java/com/alis/lotcion/models/Lot.kt
+++ app/src/main/java/com/alis/lotcion/models/Lot.kt
@@ -1,5 +1,7 @@
 package com.alis.lotcion.models
 
+import com.alis.lotcion.R
+
 data class Lot(
     var image: Int?,
     var name: String?,
@@ -10,3 +12,128 @@ data class Lot(
     var bidders: MutableList<Bidder>?,
     var isLiked: Boolean = false
 )
+
+fun getMockData(): MutableList<Lot> {
+    return mutableListOf<Lot>().apply {
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+        add(
+            Lot(
+                R.drawable.ic_launcher_background,
+                "ЛОТ",
+                "ОПИСАНИЕ ЛОТА",
+                1200,
+                3400,
+                "21ч : 34м : 3с",
+                null,
+                false,
+            )
+        )
+    }
+}
