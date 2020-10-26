2a1b89b53df99254a4964bd705d473d90f976a98
macbookpro <abenov2005.uzer@gmail.com>
2020-10-26 22:19:03 +0600

    added -> fetchLots

--- app/src/main/java/com/alis/lotcion/ui/fragments/home/lots/LotsViewModel.kt
+++ app/src/main/java/com/alis/lotcion/ui/fragments/home/lots/LotsViewModel.kt
@@ -4,4 +4,39 @@ import androidx.lifecycle.ViewModel
 
 class LotsViewModel : ViewModel() {
 
+    fun fetchLots(position: Int) {
+
+        when (position) {
+            0 -> {
+
+            }
+            1 -> {
+
+            }
+            2 -> {
+
+            }
+            3 -> {
+
+            }
+            4 -> {
+
+            }
+            5 -> {
+
+            }
+            6 -> {
+
+            }
+            7 -> {
+
+            }
+            8 -> {
+
+            }
+            9 -> {
+
+            }
+        }
+    }
 }
