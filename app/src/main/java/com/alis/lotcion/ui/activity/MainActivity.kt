f57fe65955205f6141ff7f9ba74fb3e193b40bfc
macbookpro <abenov2005.uzer@gmail.com>
2020-11-14 02:03:41 +0600

    minor refactoring

--- app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alis/lotcion/ui/activity/MainActivity.kt
@@ -3,18 +3,20 @@ package com.alis.lotcion.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.alis.lotcion.R
 import com.alis.lotcion.extensions.gone
 import com.alis.lotcion.extensions.visible
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.WindowInsetsController as WindowInsetsController
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNav: BottomNavigationView
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,34 +27,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavController() {
-        bottomNav = findViewById(R.id.bottom_nav)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        bottomNav.setupWithNavController(navController)
+        bottom_nav.setupWithNavController(navController)
         addNavControllerDestinationChangedListener()
     }
 
     private fun addNavControllerDestinationChangedListener() {
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            when (destination.id) {
-                R.id.signInFragment,
-                R.id.signUpFragment,
-                R.id.verifyCodeFragment,
-                R.id.editProfileFragment,
-                R.id.settingsFragment -> {
-                    bottomNav.gone()
-                }
-                R.id.navigation_home,
-                R.id.navigation_add_lot,
-                R.id.navigation_profile -> {
-                    bottomNav.visible()
-                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-                }
-                R.id.lotFragment -> {
-                    bottomNav.gone()
-                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-                }
+            changedLogicBottomNavigation(destination.id)
+            changedLogicStatusBar(destination.id)
+        }
+    }
+
+    private fun changedLogicBottomNavigation(id: Int) {
+        when (id) {
+            R.id.signInFragment,
+            R.id.signUpFragment,
+            R.id.verifyCodeFragment,
+            R.id.lotFragment,
+            R.id.editProfileFragment,
+            R.id.settingsFragment -> {
+                bottom_nav.gone()
+            }
+            R.id.navigation_home,
+            R.id.navigation_add_lot -> {
+                bottom_nav.visible()
+            }
+        }
+    }
+
+    private fun changedLogicStatusBar(id: Int) {
+        when (id) {
+            R.id.navigation_home,
+            R.id.navigation_profile -> {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            }
+            R.id.lotFragment -> {
+                window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+                )
             }
         }
     }
