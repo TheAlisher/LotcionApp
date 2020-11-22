a903b1e32755e1db2e38975643d84b950a6f49b9
macbookpro <abenov2005.uzer@gmail.com>
2020-11-23 03:04:42 +0600

    Created

---
+++ app/src/main/java/com/alis/lotcion/Constants.kt
@@ -0,0 +1,15 @@
+package com.alis.lotcion
+
+object Constants {
+
+    //region LotCategory
+    const val LOT_CATEGORY_ART = "Исскуство"
+    const val LOT_CATEGORY_BOOK = "Книги"
+    const val LOT_CATEGORY_HANDIWORK = "Рукоделие"
+    const val LOT_CATEGORY_PROPERTY = "Недвижимость"
+    const val LOT_CATEGORY_TRANSPORT = "Транспорт"
+    const val LOT_CATEGORY_ACCESSORY = "Аксессуары"
+    const val LOT_CATEGORY_ELECTRONICS = "Электроника"
+    const val LOT_CATEGORY_ANIMAL = "Животные"
+    //endregion
+}
